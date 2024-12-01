67b8ecd2857aea3f78f1e388557ae3d95e28d415
BlazingPh0enix <anaskazi6789@gmail.com>
2024-12-01 12:54:43 +0530

    Added secure access

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase Config
 export const firebaseConfig = {
-  apiKey: "AIzaSyCsOTWHV8zkhzFYZ1imCW_Urxza4xfC6Qg",
-  authDomain: "omniplex-5483a.firebaseapp.com",
-  projectId: "omniplex-5483a",
-  storageBucket: "omniplex-5483a.firebasestorage.app",
-  messagingSenderId: "428070744644",
-  appId: "1:428070744644:web:6ee4428525603286fe0211",
-  measurementId: "G-JEEHS4B6GW"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
